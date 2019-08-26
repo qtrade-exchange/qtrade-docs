195a33b7bf39aeb09d2ba4f1032b1bc0f854290a
Eric Cook <eric@qtrade.io>
2019-08-26 18:49:36 -0500

    Add PHP example

--- template_proc.py
+++ template_proc.py
@@ -28,6 +28,15 @@ def json_to_python_dict(value):
     value = json.loads(value)
     return pprint.pformat(value, indent=2)
 
+def json_to_php_array(value):
+    value = json.loads(value)
+    php_str = "array(\n"
+    for k,v in value.items():
+        v_str = str(v) if type(v) is int else "\"" + str(v) + "\""
+        php_str += "    \"" + str(k) + "\" => " + v_str + ",\n"
+    php_str += ")"
+    return php_str
+
 env = jinja2.Environment(
     loader=jinja2.FileSystemLoader('.'),
     autoescape=jinja2.select_autoescape(['html', 'xml']),
@@ -37,6 +46,7 @@ env = jinja2.Environment(
 env.filters['apply_env'] = apply_env
 env.filters['to_pretty_json'] = to_pretty_json
 env.filters['json_to_python_dict'] = json_to_python_dict
+env.filters['json_to_php_array'] = json_to_php_array
 
 public = [item for item in data['item'] if item['request']['url']['path'][1] != "user"]
 private = [item for item in data['item'] if item['request']['url']['path'][1] == "user"]
